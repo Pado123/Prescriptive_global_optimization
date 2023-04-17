468df2607c5cc18a9fbe676b32348ed34847c1b4
AlessandroPadella <59849186+Pado123@users.noreply.github.com>
2023-04-17 17:14:58 +0200

    Update slice_df.py

--- slice_df.py
+++ slice_df.py
@@ -19,6 +19,6 @@ for idx in df['case:concept:name'].unique():
     trace = df[df['case:concept:name']==idx].reset_index(drop=True)
     trace = trace.iloc[:round(len(trace)*thrs)]
     df_cut = df_cut.append(trace)
-import ipdb; ipdb.set_trace()
+    
 log = pm4py.convert_to_event_log(df_cut)
 pm4py.write_xes(log, f'{filename}_cut_{str(thrs)}_.xes')
